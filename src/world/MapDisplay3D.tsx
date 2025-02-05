1d1035f164053f792df80499bae6dffb818aafb4
John M <entity.john@gmail.com>
2025-02-05 12:46:43 -0600

    disable file autoload

--- src/world/MapDisplay3D.tsx
+++ src/world/MapDisplay3D.tsx
@@ -51,86 +51,86 @@ export default function MapDisplay3D({
   const searchString = useSearch();
 
   // USE EFFECT: automatically load up map from URL, OR from file
-  React.useEffect(() => {
-    const queryParams = new URLSearchParams(searchString)
-    const urlMapString = queryParams.get('m')
-    if (urlMapString) {
-      try {
-        const data = JSON.parse(JSONCrush.uncrush(urlMapString))
-        const [hexMap, ...pieceIds] = data
-        const boardPieces: BoardPieces = pieceIds.reduce(
-          (prev: BoardPieces, curr: string) => {
-            // get inventory id from pieceID (a~q~r~rot~id)
-            prev[curr] = decodePieceID(curr).pieceID as Pieces
-            return prev
-          },
-          {},
-        )
-        const jsonMap = buildupJsonFileMap(boardPieces, hexMap)
-        if (!jsonMap.hexMap.name) {
-          jsonMap.hexMap.name = genRandomMapName()
-        }
-        loadMap(jsonMap)
-        enqueueSnackbar({
-          message: `Loaded map from URL: ${jsonMap.hexMap.name}.`,
-          variant: 'success',
-          autoHideDuration: 5000,
-        })
-        // enqueueSnackbar({
-        //   message: `Map data has been removed from your URL bar, to return it please press the back button in your browser.`,
-        //   variant: 'info',
-        //   autoHideDuration: 6000,
-        // })
-        // navigate(ROUTES.heroscapeHome)
-        clearUndoHistory() // clear undo history, initial load should not be undoable
-      } catch (error: any) {
-        enqueueSnackbar({
-          message: `Error loading map from URL: ${error?.message ?? error}`,
-          variant: 'error',
-          autoHideDuration: 5000,
-        })
-        console.error('🚀 ~ React.useEffect ~ error:', error)
-      }
-    } else {
-      // AUTO VSCAPE
-      // const fileName = '/ladders.hsc'
-      // fetch(fileName)
-      //   .then((response) => {
-      //     return response.arrayBuffer()
-      //   })
-      //   .then((arrayBuffer) => {
-      //     const vsFileData = processVirtualScapeArrayBuffer(arrayBuffer)
-      //     // buildupVSFileMap should return errorArr for enqueueSnackbar
-      //     const vsMap = buildupVSFileMap(
-      //       vsFileData.tiles,
-      //       vsFileData?.name ?? fileName,
-      //     )
-      //     loadMap(vsMap)
-      //     enqueueSnackbar(
-      //       `Automatically loaded Virtualscape map named: "${vsMap.hexMap.name}" from file: "${fileName}"`,
-      //     )
-      //   })
-      // AUTO JSON
-      const fileName = '/Everything_Bagel.json'
-      fetch(fileName).then(async (response) => {
-        // const data = response.json()
-        const data = await response.json()
-        const jsonMap = buildupJsonFileMap(data.boardPieces, data.hexMap)
-        if (!jsonMap.hexMap.name) {
-          jsonMap.hexMap.name = fileName
-        }
-        loadMap(jsonMap)
-        enqueueSnackbar({
-          // message: `Loaded map "${jsonMap.hexMap.name}" from file: "${fileName}"`,
-          message: `WELCOME!`,
-          variant: 'success',
-          autoHideDuration: 5000,
-        })
-        clearUndoHistory() // clear undo history, initial load should not be undoable
-      })
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  // React.useEffect(() => {
+  //   const queryParams = new URLSearchParams(searchString)
+  //   const urlMapString = queryParams.get('m')
+  //   if (urlMapString) {
+  //     try {
+  //       const data = JSON.parse(JSONCrush.uncrush(urlMapString))
+  //       const [hexMap, ...pieceIds] = data
+  //       const boardPieces: BoardPieces = pieceIds.reduce(
+  //         (prev: BoardPieces, curr: string) => {
+  //           // get inventory id from pieceID (a~q~r~rot~id)
+  //           prev[curr] = decodePieceID(curr).pieceID as Pieces
+  //           return prev
+  //         },
+  //         {},
+  //       )
+  //       const jsonMap = buildupJsonFileMap(boardPieces, hexMap)
+  //       if (!jsonMap.hexMap.name) {
+  //         jsonMap.hexMap.name = genRandomMapName()
+  //       }
+  //       loadMap(jsonMap)
+  //       enqueueSnackbar({
+  //         message: `Loaded map from URL: ${jsonMap.hexMap.name}.`,
+  //         variant: 'success',
+  //         autoHideDuration: 5000,
+  //       })
+  //       // enqueueSnackbar({
+  //       //   message: `Map data has been removed from your URL bar, to return it please press the back button in your browser.`,
+  //       //   variant: 'info',
+  //       //   autoHideDuration: 6000,
+  //       // })
+  //       // navigate(ROUTES.heroscapeHome)
+  //       clearUndoHistory() // clear undo history, initial load should not be undoable
+  //     } catch (error: any) {
+  //       enqueueSnackbar({
+  //         message: `Error loading map from URL: ${error?.message ?? error}`,
+  //         variant: 'error',
+  //         autoHideDuration: 5000,
+  //       })
+  //       console.error('🚀 ~ React.useEffect ~ error:', error)
+  //     }
+  //   } else {
+  //     // AUTO VSCAPE
+  //     // const fileName = '/ladders.hsc'
+  //     // fetch(fileName)
+  //     //   .then((response) => {
+  //     //     return response.arrayBuffer()
+  //     //   })
+  //     //   .then((arrayBuffer) => {
+  //     //     const vsFileData = processVirtualScapeArrayBuffer(arrayBuffer)
+  //     //     // buildupVSFileMap should return errorArr for enqueueSnackbar
+  //     //     const vsMap = buildupVSFileMap(
+  //     //       vsFileData.tiles,
+  //     //       vsFileData?.name ?? fileName,
+  //     //     )
+  //     //     loadMap(vsMap)
+  //     //     enqueueSnackbar(
+  //     //       `Automatically loaded Virtualscape map named: "${vsMap.hexMap.name}" from file: "${fileName}"`,
+  //     //     )
+  //     //   })
+  //     // AUTO JSON
+  //     const fileName = '/Everything_Bagel.json'
+  //     fetch(fileName).then(async (response) => {
+  //       // const data = response.json()
+  //       const data = await response.json()
+  //       const jsonMap = buildupJsonFileMap(data.boardPieces, data.hexMap)
+  //       if (!jsonMap.hexMap.name) {
+  //         jsonMap.hexMap.name = fileName
+  //       }
+  //       loadMap(jsonMap)
+  //       enqueueSnackbar({
+  //         // message: `Loaded map "${jsonMap.hexMap.name}" from file: "${fileName}"`,
+  //         message: `WELCOME!`,
+  //         variant: 'success',
+  //         autoHideDuration: 5000,
+  //       })
+  //       clearUndoHistory() // clear undo history, initial load should not be undoable
+  //     })
+  //   }
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [])
 
   const instanceBoardHexes = getInstanceBoardHexes(boardHexesArr, isTakingPicture)
 
