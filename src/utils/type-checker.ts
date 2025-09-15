7cddc100ccebb86bed38287ab40a524407ab5d58
John M <entity.john@gmail.com>
2025-09-15 18:33:09 -0500

    fix loading from url, bug from new parsing fn

--- src/utils/type-checker.ts
+++ src/utils/type-checker.ts
@@ -4,9 +4,8 @@ export function isHexMap(item: unknown): item is HexMap {
   return (
     typeof item === 'object' &&
     item !== null &&
-    'name' in item &&
     'id' in item &&
-    'author' in item &&
+    'shape' in item &&
     'length' in item &&
     'width' in item
   )
