d40ad9bf7e87e5793222ef2002d911a4eccaf07b
John M <entity.john@gmail.com>
2025-10-03 13:07:47 -0500

    comment on frameloop demand

--- src/store/ui-slice.ts
+++ src/store/ui-slice.ts
@@ -247,7 +247,7 @@ const createUISlice: StateCreator<
         s.isTopOutlinedInterlockHexes = b
       }),
     ),
-  isFrameloopDemand: false,
+  isFrameloopDemand: false,  // app seems to lockup on stale window regardless
   toggleIsFrameloopDemand: (b: boolean) =>
     set(
       produce((s) => {
