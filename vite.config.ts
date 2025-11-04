725899fb3e8a1aefb30593665f7da43d572a0c8b
John M <entity.john@gmail.com>
2025-11-04 14:11:39 -0600

    add sentry sourcemap config
    npx @sentry/wizard@latest -i sourcemaps

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "hexoscape",
+    project: "hexoscape"
+  })],
+
+  build: {
+    sourcemap: true
+  }
 })
