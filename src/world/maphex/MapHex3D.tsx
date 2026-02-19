10a27a0044d3fe190ce0e4f23620444c5e52481c
John M <entity.john@gmail.com>
2026-02-18 21:49:04 -1000

    show Billboard over named glyphs with title

--- src/world/maphex/MapHex3D.tsx
+++ src/world/maphex/MapHex3D.tsx
@@ -48,6 +48,8 @@ import { MarvelRuin } from '../models/MarvelRuin'
 import LaurWallTrianglePillar from '../models/LaurTrianglePillar'
 import { HexCapIDDisplay } from './HexCapIDDisplay'
 import { FortifiedWall } from '../models/FortifiedWall'
+import { Billboard, Text } from '@react-three/drei'
+import { piecesSoFar } from '../../data/pieces'
 
 export const MapHex3D = ({
   boardHex,
@@ -111,6 +113,7 @@ export const MapHex3D = ({
   const isGlacier1Hex = inventoryID === Pieces.glacier1 && isObstacleHex
   const isPowerGlyphHex = boardHex.terrain === HexTerrain.glyphPower
   const isTreasureGlyphHex = boardHex.terrain === HexTerrain.glyphTreasure
+  const isNamedGlyph = (isPowerGlyphHex || isTreasureGlyphHex) && (boardHex.inventoryID !== Pieces.glyphPower && boardHex.inventoryID !== Pieces.glyphTreasure)
   const isOutcrop1Hex = inventoryID === Pieces.outcrop1 && isObstacleHex
   const isOutcrop3Hex =
     inventoryID === Pieces.outcrop3 && boardHex.isObstacleOrigin
@@ -192,10 +195,10 @@ export const MapHex3D = ({
             new Vector3(
               x,
               y -
-                HEXGRID_HEX_HEIGHT +
-                (isFluidTerrainHex(boardHex.terrain)
-                  ? HEXGRID_HEXCAP_FLUID_HEIGHT
-                  : HEXGRID_HEX_HEIGHT),
+              HEXGRID_HEX_HEIGHT +
+              (isFluidTerrainHex(boardHex.terrain)
+                ? HEXGRID_HEXCAP_FLUID_HEIGHT
+                : HEXGRID_HEX_HEIGHT),
               z,
             )
           }
@@ -370,14 +373,30 @@ export const MapHex3D = ({
       )}
       {/* POWER GLYPHS */}
       {(isPowerGlyphHex || isTreasureGlyphHex) && (
-        <group
-          position={[x, isUnderHexFluid ? yGlyphFluidUnder : yGlyph, z]}
-          rotation={[0, (boardHex.pieceRotation * -Math.PI) / 3, 0]}
-        >
-          <Suspense fallback={<ModelLoader />}>
-            <GlyphModel boardHex={boardHex} terrain={boardHex.terrain} />
-          </Suspense>
-        </group>
+        <>
+          {isNamedGlyph && (
+            <Billboard
+              position={[
+                x,
+                // position.y + (boardHex?.obstacleHeight ?? 0) * HEXGRID_HEX_HEIGHT,
+                (isUnderHexFluid ? yGlyphFluidUnder : yGlyph) + HEXGRID_HEX_HEIGHT / 3,
+                z,
+              ]}
+            >
+              <Text fontSize={0.14} color={'white'}>
+                {piecesSoFar?.[boardHex.inventoryID]?.title}
+              </Text>
+            </Billboard>
+          )}
+          <group
+            position={[x, isUnderHexFluid ? yGlyphFluidUnder : yGlyph, z]}
+            rotation={[0, (boardHex.pieceRotation * -Math.PI) / 3, 0]}
+          >
+            <Suspense fallback={<ModelLoader />}>
+              <GlyphModel isNamedGlyph={isNamedGlyph} boardHex={boardHex} terrain={boardHex.terrain} />
+            </Suspense>
+          </group>
+        </>
       )}
       {isGlacier1Hex && (
         <>
