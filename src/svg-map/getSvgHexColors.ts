6e8222fea3842533a2c962ce2f599f3f8abde549
John M <entity.john@gmail.com>
2026-02-19 20:29:28 -1000

    restore original colors for pdf view, not new 2D colors (roadwall, SZ)

--- src/svg-map/getSvgHexColors.ts
+++ src/svg-map/getSvgHexColors.ts
@@ -16,6 +16,7 @@ import {
 import { decodePieceID } from '../utils/map-utils'
 import {
   hexTerrainColor,
+  pdfColors,
   svgColors,
   svgSubLevelColors,
   virtualscapeTileColors,
@@ -196,6 +197,66 @@ export const getSvgHexSubLevelBorderColor = (
   return 'black'
 }
 
+export const getPdfHexFillColor = (hex: BoardHex | DecodedPieceID) => {
+  if (
+    isSolidTerrainHex(hex.terrain) ||
+    isFluidTerrainHex(hex.terrain) ||
+    hex.terrain === HexTerrain.laurWall ||
+    hex.terrain === HexTerrain.laurWallAddon ||
+    hex.terrain === HexTerrain.fortifiedWall ||
+    hex.terrain === HexTerrain.roadWall ||
+    hex.terrain === HexTerrain.glyphPower ||
+    hex.terrain === HexTerrain.glyphTreasure ||
+    hex.terrain === HexTerrain.tree ||
+    hex.terrain === HexTerrain.battlement ||
+    hex.terrain === HexTerrain.hive ||
+    hex.terrain === HexTerrain.ladder
+  ) {
+    return (
+      pdfColors?.[hex.terrain as keyof typeof pdfColors] ??
+      virtualscapeTileColors[hex.terrain as keyof typeof virtualscapeTileColors]
+    )
+  }
+  // StartZone: virtualscape colors, might be other designs
+  if (hex.terrain === HexTerrain.startZone) {
+    return pdfColors?.[hex.inventoryID as keyof typeof pdfColors]
+  }
+  if (hex.terrain === HexTerrain.brush) {
+    return pdfColors.fillJungle
+  }
+  if (hex.terrain === HexTerrain.marvelRuin) {
+    return pdfColors.castleWall
+  }
+  if (hex.terrain === HexTerrain.palm) {
+    // Renegade shows brush and palm as same color
+    return pdfColors.fillJungle // renegade-hexoscape
+  }
+  if (hex.terrain === HexTerrain.ruin) {
+    return pdfColors.ruin
+  }
+  if (isEvergreenTree(hex.terrain)) {
+    return pdfColors.tree
+  }
+  if (hex.terrain === HexTerrain.hive) {
+    return pdfColors.swampWater
+  }
+  if (hex.terrain === HexTerrain.glacier) {
+    return pdfColors.ice
+  }
+  if (hex.terrain === HexTerrain.outcrop) {
+    return pdfColors.outcrop
+  }
+  if (hex.terrain === HexTerrain.lavaRockOutcrop) {
+    return pdfColors.lava
+  }
+  if (hex.terrain === HexTerrain.castleWall) {
+    return pdfColors.castleWall
+  }
+  if (hex.terrain === HexTerrain.castleBase) {
+    return pdfColors.castleBase
+  }
+  return 'transparent'
+}
 export const getSvgHexFillColor = (hex: BoardHex | DecodedPieceID) => {
   if (
     isSolidTerrainHex(hex.terrain) ||
