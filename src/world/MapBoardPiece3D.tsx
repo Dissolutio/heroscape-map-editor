f2511a61488857c9e03558078a4ee09169f8545e
John M <entity.john@gmail.com>
2026-06-18 09:18:38 -0600

    fix rope ladder altitude

--- src/world/MapBoardPiece3D.tsx
+++ src/world/MapBoardPiece3D.tsx
@@ -183,7 +183,7 @@ export const MapBoardPiece3D = ({
   if (inventoryID === Pieces.ropeLadder) {
     return (
       <group
-        position={[x, y + HEXGRID_HEX_HEIGHT, z]}
+        position={[x, y, z]}
         rotation={[0, (rotation * -Math.PI) / 3, 0]}
       >
         <Suspense fallback={<ModelLoader />}>
@@ -318,10 +318,10 @@ export const MapBoardPiece3D = ({
           x,
           // either gets moved down to fluid level, or up to solid cap level
           y -
-            HEXGRID_HEX_HEIGHT -
-            (isUnderHexFluid
-              ? HEXGRID_HEX_HEIGHT - HEXGRID_HEXCAP_FLUID_HEIGHT
-              : -HEXGRID_HEXCAP_HEIGHT / 2),
+          HEXGRID_HEX_HEIGHT -
+          (isUnderHexFluid
+            ? HEXGRID_HEX_HEIGHT - HEXGRID_HEXCAP_FLUID_HEIGHT
+            : -HEXGRID_HEXCAP_HEIGHT / 2),
           z,
         ]}
         rotation={[0, getObstaclRotation(rotation), 0]}
@@ -340,10 +340,10 @@ export const MapBoardPiece3D = ({
           x,
           // either gets moved down to fluid level, or up to solid cap level
           y -
-            HEXGRID_HEX_HEIGHT -
-            (isUnderHexFluid
-              ? HEXGRID_HEX_HEIGHT - HEXGRID_HEXCAP_FLUID_HEIGHT
-              : -HEXGRID_HEXCAP_HEIGHT / 2),
+          HEXGRID_HEX_HEIGHT -
+          (isUnderHexFluid
+            ? HEXGRID_HEX_HEIGHT - HEXGRID_HEXCAP_FLUID_HEIGHT
+            : -HEXGRID_HEXCAP_HEIGHT / 2),
           z,
         ]}
         rotation={[0, getObstaclRotation(rotation), 0]}
@@ -442,7 +442,7 @@ export const MapBoardPiece3D = ({
             position={[
               x,
               (isUnderHexFluid ? yGlyphFluidUnder : yGlyph) +
-                HEXGRID_HEX_HEIGHT / 3,
+              HEXGRID_HEX_HEIGHT / 3,
               z,
             ]}
           >
@@ -626,10 +626,10 @@ export const MapBoardPiece3D = ({
           x + getLadderBattlementOptions(rotation).xAdd,
           // either gets moved down to fluid level, or up to solid cap level
           y -
-            HEXGRID_HEX_HEIGHT -
-            (isLadderChainOnFluid
-              ? HEXGRID_HEX_HEIGHT - HEXGRID_HEXCAP_FLUID_HEIGHT
-              : -HEXGRID_HEXCAP_HEIGHT / 2),
+          HEXGRID_HEX_HEIGHT -
+          (isLadderChainOnFluid
+            ? HEXGRID_HEX_HEIGHT - HEXGRID_HEXCAP_FLUID_HEIGHT
+            : -HEXGRID_HEXCAP_HEIGHT / 2),
           z + getLadderBattlementOptions(rotation).zAdd,
         ]}
         rotation={[0, pieceRotation, 0]}
