4260e8dab7b7505300e129638ee188e4ae9136eb
John M <entity.john@gmail.com>
2026-06-24 17:41:34 -0600

    fix snow tree height

--- src/data/pieces.ts
+++ src/data/pieces.ts
@@ -1137,7 +1137,7 @@ export const piecesSoFar: Dictionary<Piece> = {
     isObstaclePiece: true,
     size: 1,
     template: '1',
-    height: 9,
+    height: 10,
   },
   [Pieces.snowTree12]: {
     id: Pieces.snowTree12,
@@ -1147,7 +1147,7 @@ export const piecesSoFar: Dictionary<Piece> = {
     isObstaclePiece: true,
     size: 1,
     template: '1',
-    height: 11,
+    height: 12,
   },
   [Pieces.tree10]: {
     id: Pieces.tree10,
@@ -1554,40 +1554,40 @@ export const piecesSoFar: Dictionary<Piece> = {
   },
 }
 
-// Dynamically add specific glyph inventory entries for all defined glyphs.
-// Inventory key will be PiecePrefixes.glyph + glyph.id (e.g. 'yattack').
-;(function addGlyphs() {
-  const allGlyphArrays = [
-    powerGlyphs,
-    treasureGlyphs,
-    marvelGlyphs,
-    c3vGlyphs,
-    c3vPlaytestGlyphs,
-    customGlyphs,
-  ]
-  for (const glyphArr of allGlyphArrays) {
-    for (const g of glyphArr) {
-      try {
-        const key = `${PiecePrefixes.glyph}${g.id}`
-        // Avoid overwriting existing entries
-        if (piecesSoFar[key]) continue
-        // objective type glyphs will be
-        const terrain = g.terrain
-        piecesSoFar[key] = {
-          id: key,
-          title: g.name ?? g.shortName ?? g.id,
-          terrain,
-          isHexTerrainPiece: false,
-          isObstaclePiece: true,
-          isOverlayPiece: true,
-          size: 1,
-          template: '1',
-          height: 0,
-          glyphLetter: g.glyphLetter,
+  // Dynamically add specific glyph inventory entries for all defined glyphs.
+  // Inventory key will be PiecePrefixes.glyph + glyph.id (e.g. 'yattack').
+  ; (function addGlyphs() {
+    const allGlyphArrays = [
+      powerGlyphs,
+      treasureGlyphs,
+      marvelGlyphs,
+      c3vGlyphs,
+      c3vPlaytestGlyphs,
+      customGlyphs,
+    ]
+    for (const glyphArr of allGlyphArrays) {
+      for (const g of glyphArr) {
+        try {
+          const key = `${PiecePrefixes.glyph}${g.id}`
+          // Avoid overwriting existing entries
+          if (piecesSoFar[key]) continue
+          // objective type glyphs will be
+          const terrain = g.terrain
+          piecesSoFar[key] = {
+            id: key,
+            title: g.name ?? g.shortName ?? g.id,
+            terrain,
+            isHexTerrainPiece: false,
+            isObstaclePiece: true,
+            isOverlayPiece: true,
+            size: 1,
+            template: '1',
+            height: 0,
+            glyphLetter: g.glyphLetter,
+          }
+        } catch {
+          // ignore errors here
         }
-      } catch {
-        // ignore errors here
       }
     }
-  }
-})()
+  })()
