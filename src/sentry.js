2946164d4f8626c8255649c07625dcc19970fae5
John M <entity.john@gmail.com>
2026-08-30 13:09:00 -0600

    disable session replay, it never worked for ThreeJS Canvas anyways

--- src/sentry.js
+++ src/sentry.js
@@ -2,7 +2,7 @@ import * as Sentry from '@sentry/react'
 
 Sentry.init({
   dsn: 'https://abf54ce42681460cbea61d773becd43d@o374574.ingest.us.sentry.io/5192792',
-  integrations: [Sentry.replayIntegration()],
+  // integrations: [Sentry.replayIntegration()],
 
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for tracing.
@@ -13,10 +13,10 @@ Sentry.init({
   // Set `tracePropagationTargets` to control for which URLs trace propagation should be enabled
   tracePropagationTargets: [/^\//, /^https:\/\/yourserver\.io\/api/],
 
-  // Capture Replay for 10% of all sessions,
-  // plus for 100% of sessions with an error
-  // Learn more at
-  // https://docs.sentry.io/platforms/javascript/session-replay/configuration/#general-integration-configuration
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
+  // // Capture Replay for 10% of all sessions,
+  // // plus for 100% of sessions with an error
+  // // Learn more at
+  // // https://docs.sentry.io/platforms/javascript/session-replay/configuration/#general-integration-configuration
+  // replaysSessionSampleRate: 0.1,
+  // replaysOnErrorSampleRate: 1.0,
 })
