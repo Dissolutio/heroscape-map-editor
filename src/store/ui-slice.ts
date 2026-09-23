1c8991959edc25ce684afd36b19dab9c3eb4fc0a
John M <entity.john@gmail.com>
2026-09-23 15:06:54 -0600

    integrate new pdf format into zustand state

--- src/store/ui-slice.ts
+++ src/store/ui-slice.ts
@@ -112,8 +112,10 @@ export interface UISlice {
   toggleIsShowPdfLevelLogo: (b: boolean) => void
   isShowPdfTileLetters: boolean
   toggleIsShowPdfTileLetters: (b: boolean) => void
-  pdfRenderFormat: 'coversheet' | 'shortHeader'
-  setPdfRenderFormat: (format: 'coversheet' | 'shortHeader') => void
+  pdfRenderFormat: 'coversheet' | 'shortHeader' | 'condensedCoversheet'
+  setPdfRenderFormat: (
+    format: 'coversheet' | 'shortHeader' | 'condensedCoversheet',
+  ) => void
   // SVG STATE
   is2DOverlayLevelEnabled: boolean
   toggleIs2DOverlayLevelEnabled: (b: boolean) => void
@@ -409,7 +411,9 @@ const createUISlice: StateCreator<
       }),
     ),
   pdfRenderFormat: 'coversheet',
-  setPdfRenderFormat: (format: 'coversheet' | 'shortHeader') =>
+  setPdfRenderFormat: (
+    format: 'coversheet' | 'shortHeader' | 'condensedCoversheet',
+  ) =>
     set(
       produce((s) => {
         s.pdfRenderFormat = format
