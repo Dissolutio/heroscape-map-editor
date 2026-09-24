8922847777648376ac57d2a0088b7a31fcc04430
John M <entity.john@gmail.com>
2026-09-24 12:32:51 -0600

    update sentry/tauri integration for desktop and source maps versions

--- src/sentry.js
+++ src/sentry.js
@@ -1,22 +1,13 @@
 import * as Sentry from '@sentry/react'
-
 Sentry.init({
   dsn: 'https://abf54ce42681460cbea61d773becd43d@o374574.ingest.us.sentry.io/5192792',
-  // integrations: [Sentry.replayIntegration()],
 
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for tracing.
-  // Learn more at
-  // https://docs.sentry.io/platforms/javascript/configuration/options/#traces-sample-rate
-  tracesSampleRate: 1.0,
+  // Only turn Sentry on for production builds
+  enabled: !import.meta.env.DEV,
 
-  // Set `tracePropagationTargets` to control for which URLs trace propagation should be enabled
-  tracePropagationTargets: [/^\//, /^https:\/\/yourserver\.io\/api/],
+  // Whitelist Tauri v2 production environment protocols
+  allowUrls: [/tauri\.localhost/, /tauri:/],
 
-  // // Capture Replay for 10% of all sessions,
-  // // plus for 100% of sessions with an error
-  // // Learn more at
-  // // https://docs.sentry.io/platforms/javascript/session-replay/configuration/#general-integration-configuration
-  // replaysSessionSampleRate: 0.1,
-  // replaysOnErrorSampleRate: 1.0,
+  // Link errors to your GitHub Git Tag release version
+  release: `hexoscape@${import.meta.env.VITE_RELEASE_VERSION || 'local-development'}`,
 })
